bfb1591517dcb67d220f8eca2a683e9374013c18
rikilega <rosedoland33@yahoo.com>
2023-08-13 22:02:20 -0700

    modified server.js

--- server/server.js
+++ server/server.js
@@ -2,18 +2,28 @@
 
 require('dotenv').config({ path: '../.env' });
 const express = require('express');
+const path = require('path');
 const connectDB = require('./config/database');
-const usersRoutes = require('./routes/api/users')
+const usersRoutes = require('./routes/api/users');
+
 const app = express();
 
-app.use(express.json());
-
-app.use('/api/users', usersRoutes);
 // Connect to MongoDB
 connectDB();
 
 // Middleware
 app.use(express.json({ extended: false }));
+
+app.use('/api/users', usersRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 
